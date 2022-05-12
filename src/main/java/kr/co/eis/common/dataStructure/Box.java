64fcf30715255e9e168a48ed2a4239afddc30fc5
MinhyeSim <smine0032@gmail.com>
2022-05-12 16:54:35 +0900

    16:54 제네릭 클래스

---
+++ src/main/java/kr/co/eis/common/dataStructure/Box.java
@@ -0,0 +1,15 @@
+package kr.co.eis.common.dataStructure;
+
+/**
+ * packageName: kr.co.eis.common.dataStructure
+ * fileName   : Box
+ * author     : MinHye_Sim
+ * date       : 2022-05-12
+ * desc       :
+ * ================================
+ * DATE        AUTHOR        NOTE
+ * ================================
+ * 2022-05-12   MinHye_Sim   최초 생성
+ */
+public class Box<T>{
+}
