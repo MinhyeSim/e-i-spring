57a47f42f752b8b577b349a11327e5641002ad8e
MinhyeSim <smine0032@gmail.com>
2022-05-12 17:46:22 +0900

    17:46 중간 커밋

--- src/main/java/kr/co/eis/common/dataStructure/Box.java
+++ src/main/java/kr/co/eis/common/dataStructure/Box.java
@@ -1,5 +1,12 @@
 package kr.co.eis.common.dataStructure;
 
+import lombok.Data;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * packageName: kr.co.eis.common.dataStructure
  * fileName   : Box
@@ -11,5 +18,14 @@ package kr.co.eis.common.dataStructure;
  * ================================
  * 2022-05-12   MinHye_Sim   최초 생성
  */
+@Component @Data @Lazy
 public class Box<T>{
+    private final ArrayList<T> list;
+    public Box(){this.list = new ArrayList<>();}
+
+    
+
+
+
+
 }
