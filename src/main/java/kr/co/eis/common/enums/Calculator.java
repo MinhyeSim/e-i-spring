de3c652412a31a75d2ff39929598b78a2831bdc5
MinhyeSim <smine0032@gmail.com>
2022-05-13 14:54:24 +0900

    14:54 중간 커밋

--- src/main/java/kr/co/eis/common/enums/Calculator.java
+++ src/main/java/kr/co/eis/common/enums/Calculator.java
@@ -1,5 +1,9 @@
 package kr.co.eis.common.enums;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.BiFunction;
+
 /**
  * packageName: kr.co.eis.common.enums
  * fileName   : Calculator
@@ -15,8 +19,13 @@ public class Calculator {
     public static void main(String[] args) {
 
     }
+    @RequiredArgsConstructor
     enum Operation{
     //enum자체가 static file(?)를 가지고 있기 때문에 별도의 static이 필요 없다
+        private final String opcode;
+        //수 먼저 구성 (숫자 2개가 들어가고 1개가 리턴)
+        private BiFunction<Integer, Integer, Integer> expression;
+        
     }
 
 
