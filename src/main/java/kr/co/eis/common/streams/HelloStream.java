f196699fb5a7e3c096c584202147e0f333be328a
MinhyeSim <smine0032@gmail.com>
2022-05-16 18:05:31 +0900

    18:04 PersonSterm 연습(이름 입력시 ssn를 통해 성별 출력)

--- src/main/java/kr/co/eis/common/streams/HelloStream.java
+++ src/main/java/kr/co/eis/common/streams/HelloStream.java
@@ -21,45 +21,42 @@ import java.util.stream.Collectors;
 
 
 public class HelloStream {
-    //@Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     @Getter
-    public static class Hello {
+    public static class Hello{
         private String greeting, inLanguage;
 
         @Override
         public String toString() {
-            return String.format(" 인사 : %s, %s", inLanguage, greeting);
-        }
-
-        interface HelloService {
-            Set<Hello> greet(List<Hello> arr);
-        }
-
-        static class HelloServiceImpl implements HelloService {
-            @Override
-            public Set<Hello> greet(List<Hello> arr) {
-                return arr
-                        .stream()
-                        .filter(e -> e.getInLanguage().equals("영어"))
-                        .collect(Collectors.toSet());
-            }
-        }
-
-        @Test
-        void helloTest() {
-            List<Hello> arr = Arrays.asList(
-                    Hello.builder().inLanguage("영어").greeting("Hello").build(),
-                    Hello.builder().inLanguage("한국어").greeting("안녕").build()
-            );
-            new HelloServiceImpl()
-                    .greet(arr)
-                    .forEach(System.out::print);
+            return String.format(" 인사 : %s, %s", inLanguage,greeting);
         }
     }
+    interface HelloService{
+        Set<Hello> greet(List<Hello> arr);
+    }
+    static class HelloServiceImpl implements HelloService{
+        @Override
+        public Set<Hello> greet(List<Hello> arr) {
+            return arr
+                    .stream()
+                    .filter(e -> e.getInLanguage().equals("영어"))
+                    .collect(Collectors.toSet());
+        }
+    }
+    @Test
+    void helloTest(){
+        List<Hello> arr = Arrays.asList(
+                Hello.builder().inLanguage("영어").greeting("Hello").build(),
+                Hello.builder().inLanguage("한국어").greeting("안녕").build()
+        );
+        new HelloServiceImpl()
+                .greet(arr)
+                .forEach(System.out::print);
+    }
 }
+
     //
 
 
