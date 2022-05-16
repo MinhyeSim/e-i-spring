ceb3811b704973f1487d579a02b75250af82a525
MinhyeSim <smine0032@gmail.com>
2022-05-16 16:26:41 +0900

    16:26 Stream end

--- src/main/java/kr/co/eis/common/streams/HelloStream.java
+++ src/main/java/kr/co/eis/common/streams/HelloStream.java
@@ -1,7 +1,6 @@
 package kr.co.eis.common.streams;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
@@ -19,54 +18,51 @@ import java.util.stream.Collectors;
  * ================================
  * 2022-05-16   MinHye_Sim   최초 생성
  */
-//한국어로 안녕, 영어로 Hello
+
 
 public class HelloStream {
-    @Data
+    //@Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
     public static class Hello {
         private String greeting, inLanguage;
 
-        public Hello(Builder builder){
-            this.greeting = builder.greeting;
-            this.inLanguage = builder.inLanguage;
-        }
-        @NoArgsConstructor
-        static public class Builder{
-            public String greeting;
-            public String inLanguage;
-
-            public Builder greeting(String greeting){this.greeting=greeting;return this;}
-            public Builder inLanguage(String inLanguage){this.inLanguage=inLanguage;return  this;}
-            public Hello build(){return new Hello(this);}
-
-        }
-    }
-    interface HelloService {
-
-        Set<String> greet (String[] arr);
-        }
-    static class HelloServiceImpl implements HelloService {
-
         @Override
-        public Set<String> greet(String[] arr) {
-            return Arrays.asList(arr)
-                    .stream()
-                    .filter(e -> e.startsWith("한국어"))
-                    .collect(Collectors.toSet());
+        public String toString() {
+            return String.format(" 인사 : %s, %s", inLanguage, greeting);
         }
-    }
 
-        @Test
-            void helloStreamTest(){
-                String[] arr = {"한국어 안녕","영어 Hello"};
-                HelloService s = new HelloServiceImpl();
-                Set<String> s2 = s.greet(arr);
-                s2.forEach(System.out::println);
+        interface HelloService {
+            Set<Hello> greet(List<Hello> arr);
+        }
 
+        static class HelloServiceImpl implements HelloService {
+            @Override
+            public Set<Hello> greet(List<Hello> arr) {
+                return arr
+                        .stream()
+                        .filter(e -> e.getInLanguage().equals("영어"))
+                        .collect(Collectors.toSet());
             }
         }
 
-    
+        @Test
+        void helloTest() {
+            List<Hello> arr = Arrays.asList(
+                    Hello.builder().inLanguage("영어").greeting("Hello").build(),
+                    Hello.builder().inLanguage("한국어").greeting("안녕").build()
+            );
+            new HelloServiceImpl()
+                    .greet(arr)
+                    .forEach(System.out::print);
+        }
+    }
+}
+    //
+
+
 
 
 
---
+++ src/main/java/kr/co/eis/common/streams/PersonStream.java
@@ -0,0 +1,17 @@
+package kr.co.eis.common.streams;
+
+/**
+ * packageName: kr.co.eis.common.streams
+ * fileName   : PersonStream
+ * author     : MinHye_Sim
+ * date       : 2022-05-16
+ * desc       :
+ * ================================
+ * DATE        AUTHOR        NOTE
+ * ================================
+ * 2022-05-16   MinHye_Sim   최초 생성
+ */
+public class PersonStream {
+    private String name, email, phone, ssn;
+
+}
