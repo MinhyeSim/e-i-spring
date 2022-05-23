081454998ad8dddf540bdee790eb714bc6eafb3d
MinhyeSim <smine0032@gmail.com>
2022-05-23 15:01:34 +0900

    15:01 SecurityConfig 추가

---
+++ src/main/java/kr/co/eis/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package kr.co.eis.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+/**
+ * packageName: kr.co.eis.config
+ * fileName   : SecurityConfig
+ * author     : MinHye_Sim
+ * date       : 2022-05-23
+ * desc       :
+ * ================================
+ * DATE        AUTHOR        NOTE
+ * ================================
+ * 2022-05-23   MinHye_Sim   최초 생성
+ */
+@RequiredArgsConstructor
+public class SecurityConfig extends SecurityConfigurerAdapter <DefaultSecurityFilterChain, HttpSecurity>{
+    //내부에서 Security
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+    }
+}
--- src/main/java/kr/co/eis/config/WebSecurityConfig.java
+++ src/main/java/kr/co/eis/config/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package kr.co.eis.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * packageName: kr.co.eis.config
@@ -19,10 +21,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    //외부에서 노출 되는 보안
     @Override
-    public void configure(WebSecurity auth) throws Exception {
+    public void configure(WebSecurity web) throws Exception {
+        //홈 화면에서 누구든지 볼 수 있게 할 것이다.
         web.ignoring()
                 .antMatchers(HttpMethod.OPTIONS,"*/**")
                 .antMatchers("/");
+    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //무상태
+        http.authorizeRequests()
+                .antMatchers("/users/join").permitAll()
+                .antMatchers("/users/login").permitAll()
+                .anyRequest().authenticated();
+        http.exceptionHandling().accessDeniedPage("/users/login");
+
+
     }
 }
