afd45dbb62312c3f7ad2e3e9b66f4e8db4b5f19d
MinhyeSim <smine0032@gmail.com>
2022-05-24 13:57:54 +0900

    13:57

--- src/main/java/kr/co/eis/security/domains/SecurityToken.java
+++ src/main/java/kr/co/eis/security/domains/SecurityToken.java
@@ -1,5 +1,12 @@
 package kr.co.eis.security.domains;
 
+import io.jsonwebtoken.*;
+import lombok.extern.java.Log;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * packageName: kr.co.eis.security.domains
  * fileName   : SecurityToken
@@ -11,5 +18,37 @@ package kr.co.eis.security.domains;
  * ================================
  * 2022-05-23   MinHye_Sim   최초 생성
  */
+@Log
 public class SecurityToken {
+    private String token;
+    private String key;
+
+    public SecurityToken(String key){
+        this.key = key;
+        this.token = createToken();
+    }
+
+    private String createToken(){
+        try{
+            Map<String, Object> headers = new HashMap<>();
+                    //해시맵
+            headers.put("typ","JWT");
+            headers.put("alg","HS256");
+            Map<String, Object> payload = new HashMap<>();
+            headers.put("data","blahblah");
+            Date ext = new Date();
+            return Jwts.builder()
+                    .setHeader(headers)
+                    .setPayload("user")
+                    .setSubject(ext)
+                    .sigWith(SignatureAlgorithm.HS256, key.getBytes())
+                    .compact();
+        }
+        catch (SecurityException e){log.info("SecurityException JWT");}
+        catch (MalformedJwtException e){log.info("MalformedJwtException JWT");}
+        catch (ExpiredJwtException e){log.info("ExpiredJwtException JWT");}
+        catch (UnsupportedJwtException e){log.info("UnsupportedJwtException JWT");}
+        catch (IllegalAccessError e){log.info("IllegalAccessError JWT");}
+        return null;
+    }
 }
--- src/main/java/kr/co/eis/security/domains/UserDetailsImpl.java
+++ src/main/java/kr/co/eis/security/domains/UserDetailsImpl.java
@@ -4,6 +4,7 @@ import com.sun.istack.NotNull;
 import lombok.Cleanup;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -31,17 +32,17 @@ import java.util.stream.Collectors;
 public class UserDetailsImpl implements UserDetails {
     private final long userid;
     private final String username;
-    private final String password;
+    @JsonIgnore private final String password;
     private final String name;
     private final String email;
 
-    /*public static UserDetailsImpl build (User user) {
+    public static UserDetailsImpl build (User user) {
         List<GrantedAuthority> authorities = user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
                 .collect(Collectors.toList());
                 return new UserDtailsImpl(user.getUserId(), user.getUsername(), user.getPassword(),
                         user.getName(), user.getEmail(), authorities);
-    }*/
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
