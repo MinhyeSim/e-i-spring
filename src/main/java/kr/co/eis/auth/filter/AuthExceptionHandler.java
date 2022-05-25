ba313282ded8f0c24ea58419bf139ba04f7a7515
MinhyeSim <smine0032@gmail.com>
2022-05-25 12:38:13 +0900

    12:38 AOP 끝

--- src/main/java/kr/co/eis/auth/filter/AuthExceptionHandler.java
+++ src/main/java/kr/co/eis/auth/filter/AuthExceptionHandler.java
@@ -1,5 +1,18 @@
 package kr.co.eis.auth.filter;
 
+import kr.co.eis.auth.domains.Messenger;
+import kr.co.eis.auth.exception.ErrorCode;
+import kr.co.eis.auth.exception.LoginRunnerException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
 /**
  * packageName: kr.co.eis.auth.filter
  * fileName   : AuthException
@@ -11,5 +24,71 @@ package kr.co.eis.auth.filter;
  * ================================
  * 2022-05-25   MinHye_Sim   최초 생성
  */
+@Slf4j
+@ControllerAdvice
 public class AuthExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<Messenger> handleRuntimeException(RuntimeException e) {
+        log.info("handleRuntimeException", e);
+        return new ResponseEntity<>(Messenger.builder()
+                .code("test").message(e.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    protected ResponseEntity<Messenger> handleSecurityException (SecurityException e){
+        log.info("handleSecurityException", e);
+        return new ResponseEntity<>(Messenger.builder()
+                .code(ErrorCode.AUTHENTICATION_FAILED.getCode())
+                .message(e.getMessage())
+                .status(ErrorCode.AUTHENTICATION_FAILED.getStatus())
+                .build(),
+                HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(LoginRunnerException.class)
+    protected ResponseEntity<Messenger> handleLoginRunnerException (LoginRunnerException e){
+        log.info("handleLoginRunnerException", e);
+        return new ResponseEntity<>(Messenger.builder()
+                .code(ErrorCode.LOGIN_FAILED.getCode())
+                .message(ErrorCode.LOGIN_FAILED.getMsg())
+                .status(ErrorCode.LOGIN_FAILED.getStatus())
+                .build(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<Messenger> handleBadCredentialsException (BadCredentialsException e){
+        log.info("handleBadCredentialsException", e);
+        return new ResponseEntity<>(Messenger.builder()
+                .code(ErrorCode.ACCESS_DENIED.getCode())
+                .message(ErrorCode.ACCESS_DENIED.getMsg())
+                .status(ErrorCode.ACCESS_DENIED.getStatus())
+                .build(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    protected ResponseEntity<Messenger> handleAccessDeniedException (AccessDeniedException e){
+        log.info("handleAccessDeniedException", e);
+        return new ResponseEntity<>(Messenger.builder()
+                .code(ErrorCode.ACCESS_DENIED.getCode())
+                .message(ErrorCode.ACCESS_DENIED.getMsg())
+                .status(ErrorCode.ACCESS_DENIED.getStatus())
+                .build(),
+                HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    protected ResponseEntity<Messenger> handleInsufficientAuthenticationException
+    (InsufficientAuthenticationException e){
+        log.info("handleAInsufficientAuthenticationException", e);
+        return new ResponseEntity<>(Messenger.builder()
+                .code(ErrorCode.AUTHENTICATION_FAILED.getCode())
+                .message(ErrorCode.AUTHENTICATION_FAILED.getMsg())
+                .status(ErrorCode.AUTHENTICATION_FAILED.getStatus())
+                .build(),
+                HttpStatus.UNAUTHORIZED);
+    }
 }
+
--- src/main/java/kr/co/eis/auth/filter/AuthFilter.java
+++ src/main/java/kr/co/eis/auth/filter/AuthFilter.java
@@ -1,5 +1,18 @@
 package kr.co.eis.auth.filter;
 
+import kr.co.eis.auth.configs.AuthProvider;
+import kr.co.eis.auth.exception.SecurityRuntimeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * packageName: kr.co.eis.auth.filter
  * fileName   : AuthFilter
@@ -11,5 +24,35 @@ package kr.co.eis.auth.filter;
  * ================================
  * 2022-05-25   MinHye_Sim   최초 생성
  */
-public class AuthFilter {
+
+@RequiredArgsConstructor
+public class AuthFilter extends OncePerRequestFilter {
+    private final AuthProvider provider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = provider.resolveToken(request); // 1. provider가 토큰을 판단 2.resolveTonken()이 처리
+        try{ //토큰에 대한 유효성을 판단
+            if(token != null && provider.validateToken(token)) {
+                //1.없으면 1차적으로 제거
+                //2.프로바이더가 판단
+                //3.validateToken()이 해당 토큰이 유효한지 확인(?)
+                Authentication auth = provider.getAuthentication(token);
+                SecurityContextHolder.clearContext();
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+            }
+        }
+        catch (SecurityRuntimeException ex) {
+            //this is very important, since it guarantees the user is not authenticated at all
+            SecurityContextHolder.clearContext();
+            response.sendError(ex.getHttpStatus().value(), ex.getMessage());
+            return;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response); //입장하는 것도 나가는 것도
+
+    }
 }
