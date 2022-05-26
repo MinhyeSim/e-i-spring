0cbef6ff096643c1c5385f22b32fde53c10d15e1
MinhyeSim <smine0032@gmail.com>
2022-05-26 10:58:55 +0900

    2022-05-26 10:58 스웨거

--- src/main/java/kr/co/eis/user/controllers/UserController.java
+++ src/main/java/kr/co/eis/user/controllers/UserController.java
@@ -1,10 +1,12 @@
 package kr.co.eis.user.controllers;
 
+import io.swagger.annotations.*;
 import kr.co.eis.auth.domains.Messenger;
 import kr.co.eis.user.domains.User;
 import kr.co.eis.user.domains.UserDTO;
 import kr.co.eis.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -25,18 +27,25 @@ import java.util.Optional;
  * ================================
  * 2022-05-03   MinHye_Sim   최초 생성
  */
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Api(tags = "")//외부에서 users라고
 @RestController // 컴포넌트다 @User의 컴포넌트의 자식이다. (컴포넌트 = 프로퍼티 + 메소드)
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService service;
+    private final ModelMapper modelMapper;
 
 
-    @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody User user) {
-        return ResponseEntity.ok(service.login(user));
+    @PostMapping("/login") //엔티티 개체를 외부로 노출하지 x
+    @ApiOperation(value = "${UserController.login}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something Wrong"),
+            @ApiResponse(code = 422, message = "유효하지 않은 아이디/ 비밀번호")
+    })
+    public ResponseEntity<UserDTO> login(@RequestBody UserDTO user) {
+        return ResponseEntity.ok(service.login(modelMapper.map(user,User.class)));
         //엔티티니까 실제 값을 보낸다
     }
 
@@ -70,17 +79,19 @@ public class UserController {
         return service.update();
     }
 
-   /* @PutMapping("/put")
-    public String put(@RequestBody User user) {
-        return service.put(user);
-    }*/
-
     @DeleteMapping("/delete")
     public String delete(@RequestBody User user) { return service.delete(user); }
 
     @PostMapping("/join")
-    public Messenger save(@RequestBody User user) {
-        return service.save(user);
+    @ApiOperation(value = "${UserController.join}")//라우터 user를 들어와서 join에들어와서 찍히고
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something Wrong"),
+            @ApiResponse(code = 403, message = "승인거절"),
+            @ApiResponse(code = 402, message = "중복된 ID")
+    })
+    public ResponseEntity<Messenger> save(@ApiParam("Join User") @RequestBody UserDTO user) {
+        System.out.println("회원가입 정보: "+user.toString());
+        return ResponseEntity.ok(service.save(modelMapper.map(user,User.class)));
     }
 
     @GetMapping("/findById/{userid}")
